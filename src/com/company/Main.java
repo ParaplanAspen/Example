5854e0d911aecff9ec41fcf926a83f03eef0e9d3
ParaplanAspen <south_w@ukr.net>
2021-10-08 17:01:20 +0300

    test2

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+/* проверка второй части для комита
+проба№2
+ */
+class Example {
+    public static void main (String args[]) {
+        int num; // объявили переменную num
+    num=100; // присвоил значение 100
+        System.out.println("Это переменная num:"  + num);
+    num=num*2;
+        System.out.print("Значение переменной num * 2 равно");
+        System.out.println(num);
+    }
+}
