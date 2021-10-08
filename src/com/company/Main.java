b87c42c08c5e68c11332608e12c2077b92bf0de5
ParaplanAspen <south_w@ukr.net>
2021-10-08 17:02:16 +0300

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,11 @@
 проба№2
  */
 class Example {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         int num; // объявили переменную num
-    num=100; // присвоил значение 100
-        System.out.println("Это переменная num:"  + num);
-    num=num*2;
+        num = 100; // присвоил значение 100
+        System.out.println("Это переменная num:" + num);
+        num = num * 2;
         System.out.print("Значение переменной num * 2 равно");
         System.out.println(num);
     }
